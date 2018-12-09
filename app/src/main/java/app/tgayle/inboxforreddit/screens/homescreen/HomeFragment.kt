bbd7b71d70296c93c1ce9c17177d2729345892d6
tgayle <travisgayledev@gmail.com>
2018-12-09 18:30:21 -0500

    HomeFragment layout plans

--- app/src/main/java/app/tgayle/inboxforreddit/screens/homescreen/HomeFragment.kt
+++ app/src/main/java/app/tgayle/inboxforreddit/screens/homescreen/HomeFragment.kt
@@ -29,10 +29,7 @@ class HomeFragment : Fragment() {
 
         super.onCreate(savedInstanceState)
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
         val text = view.findViewById<TextView>(R.id.homeHello)
