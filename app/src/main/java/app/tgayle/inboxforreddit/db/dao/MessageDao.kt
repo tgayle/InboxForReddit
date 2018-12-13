4ebe53eb8e82d61a0f7315891934f97361c1718d
tgayle <travisgayledev@gmail.com>
2018-12-13 18:56:51 -0500

    Refactor MessageDao

--- app/src/main/java/app/tgayle/inboxforreddit/db/dao/MessageDao.kt
+++ app/src/main/java/app/tgayle/inboxforreddit/db/dao/MessageDao.kt
@@ -10,43 +10,37 @@ import app.tgayle.inboxforreddit.model.RedditMessage
 @Dao
 abstract class MessageDao {
 
-    @Query("SELECT * FROM messages")
-    abstract fun getAllMessages(): LiveData<List<RedditMessage>>
+    companion object {
+        const val ALL_MESSAGES = "SELECT * FROM messages"
+        const val ALL_USER_MESSAGES = "SELECT * FROM messages WHERE owner = :username"
+        const val ALL_USER_SENT_MESSAGES = "SELECT * FROM messages WHERE owner = :username AND author = :username ORDER BY created DESC"
+        const val ALL_UNREAD_USER_MESSAGES = "SELECT * FROM messages WHERE owner = :username AND unread = 1"
+        const val ALL_UNREAD_USER_MESSAGES_DESC = "SELECT * FROM messages WHERE owner = :username AND unread = 1 ORDER BY created DESC"
+        const val OLDEST_UNREAD_USER_MESSAGES = "SELECT * FROM messages WHERE owner = :username AND unread = 1 ORDER BY created ASC LIMIT 1"
+        const val UPDATE_MESSAGE_UNREAD_VALUE = "UPDATE messages SET unread = :newUnread WHERE fullName = :fullName"
+        const val COUNT_USER_MESSAGES = "SELECT COUNT(*) FROM messages WHERE owner = :username"
+        const val NEWEST_USER_SENT_MESSAGE = "SELECT * FROM messages WHERE owner = :username AND author = :username ORDER BY created DESC LIMIT 1"
+        const val NEWEST_USER_RECEIVED_MESSAGE = "SELECT * FROM messages WHERE owner = :username AND destination = :username ORDER BY created DESC LIMIT 1"
 
-    @Query("SELECT * FROM messages WHERE owner = :username")
-    abstract fun getUserMessages(username: String): LiveData<List<RedditMessage>>
+        const val ALL_USER_CONVERSATIONS_DESC = """
+            SELECT *
+            FROM messages
+            WHERE owner = :username
+            GROUP BY parentName
+            HAVING created = MAX(created)
+            ORDER BY created DESC
+        """
 
-    @Query("SELECT * FROM messages WHERE owner = :username AND author = :username ORDER BY created DESC")
-    abstract fun getUserSentMessagesDesc(username: String): LiveData<List<RedditMessage>>
-
-    @Query("SELECT * FROM messages WHERE owner = :username AND unread = 1 ORDER BY created DESC")
-    abstract fun getUnreadMessagesDesc(username: String): LiveData<List<RedditMessage>>
-
-    @Query("""
-        SELECT *
-        FROM messages
-        WHERE owner = :username
-        GROUP BY parentName
-        HAVING created = MAX(created)
-        AND (COUNT(CASE WHEN unread = 1 then 1 end) >= 1)
-        ORDER BY created DESC
-    """)
-    abstract fun getConversationsWithUnreadMessages(username: String): LiveData<List<RedditMessage>>
-
-    @Query("SELECT * FROM messages WHERE owner = :username AND unread = 1")
-    abstract fun getUnreadMessagesSync(username: String): List<RedditMessage>
-
-    @Query("SELECT * FROM messages WHERE owner = :username AND unread = 1 ORDER BY created ASC LIMIT 1")
-    abstract fun getOldestUnreadMessageSync(username: String): RedditMessage
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    protected abstract fun insert(messages: List<RedditMessage>): List<Long>
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    protected abstract fun insert(message: RedditMessage): Long
-
-    @Query("UPDATE messages SET unread = :newUnread WHERE fullName = :fullName")
-    protected abstract fun insertReplacingOldMessage(fullName: String, newUnread: Boolean = false): Long
+        const val ALL_CONVERSATIONS_WITH_UNREAD_MESSAGES = """
+            SELECT *
+            FROM messages
+            WHERE owner = :username
+            GROUP BY parentName
+            HAVING created = MAX(created)
+            AND (COUNT(CASE WHEN unread = 1 then 1 end) >= 1)
+            ORDER BY created DESC
+        """
+    }
 
     // Insert new message or update preexisting message.
     fun upsert(messages: List<RedditMessage>): List<Long> {
@@ -64,20 +58,45 @@ abstract class MessageDao {
         return insertionResults
     }
 
-    @Query("SELECT * " +
-            "FROM messages " +
-            "WHERE owner = :username " +
-            "GROUP BY parentName " +
-            "HAVING created = MAX(created) " +
-            "ORDER BY created DESC")
+    @Query(ALL_MESSAGES)
+    abstract fun getAllMessages(): LiveData<List<RedditMessage>>
+
+    @Query(ALL_USER_MESSAGES)
+    abstract fun getUserMessages(username: String): LiveData<List<RedditMessage>>
+
+    @Query(ALL_USER_SENT_MESSAGES)
+    abstract fun getUserSentMessagesDesc(username: String): LiveData<List<RedditMessage>>
+
+    @Query(ALL_UNREAD_USER_MESSAGES_DESC)
+    abstract fun getUnreadMessagesDesc(username: String): LiveData<List<RedditMessage>>
+
+    @Query(ALL_CONVERSATIONS_WITH_UNREAD_MESSAGES)
+    abstract fun getConversationsWithUnreadMessages(username: String): LiveData<List<RedditMessage>>
+
+    @Query(ALL_UNREAD_USER_MESSAGES)
+    abstract fun getUnreadMessagesSync(username: String): List<RedditMessage>
+
+    @Query(OLDEST_UNREAD_USER_MESSAGES)
+    abstract fun getOldestUnreadMessageSync(username: String): RedditMessage
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    protected abstract fun insert(messages: List<RedditMessage>): List<Long>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    protected abstract fun insert(message: RedditMessage): Long
+
+    @Query(UPDATE_MESSAGE_UNREAD_VALUE)
+    protected abstract fun insertReplacingOldMessage(fullName: String, newUnread: Boolean = false): Long
+
+    @Query(ALL_USER_CONVERSATIONS_DESC)
     abstract fun getConversationPreviews(username: String): LiveData<List<RedditMessage>>
 
-    @Query("SELECT COUNT(*) FROM messages WHERE owner = :username")
+    @Query(COUNT_USER_MESSAGES)
     abstract fun getUserMessageCount(username: String): Int
 
-    @Query("SELECT * FROM messages WHERE owner = :username AND author = :username ORDER BY created DESC LIMIT 1")
+    @Query(NEWEST_USER_SENT_MESSAGE)
     abstract fun getNewestSentUserMessageSync(username: String): RedditMessage
 
-    @Query("SELECT * FROM messages WHERE owner = :username AND destination = :username ORDER BY created DESC LIMIT 1")
+    @Query(NEWEST_USER_RECEIVED_MESSAGE)
     abstract fun getNewestReceivedUserMessageSync(username: String): RedditMessage
 }
