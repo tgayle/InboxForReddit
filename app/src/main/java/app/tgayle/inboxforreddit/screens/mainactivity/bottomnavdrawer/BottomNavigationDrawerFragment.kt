79caccfacb1f65e34d232b3dd91873b0ec00719c
tgayle <travisgayledev@gmail.com>
2018-12-15 23:17:57 -0500

    Showing current user in BottomNavigationDrawerFragment

--- app/src/main/java/app/tgayle/inboxforreddit/screens/mainactivity/bottomnavdrawer/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/app/tgayle/inboxforreddit/screens/mainactivity/bottomnavdrawer/BottomNavigationDrawerFragment.kt
@@ -9,21 +9,29 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import app.tgayle.inboxforreddit.AppSingleton
 import app.tgayle.inboxforreddit.R
+import app.tgayle.inboxforreddit.screens.mainactivity.MainActivityViewModel
+import app.tgayle.inboxforreddit.screens.mainactivity.MainActivityViewModelFactory
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.main_bottom_nav_drawer.*
 
 class BottomNavigationDrawerFragment: BottomSheetDialogFragment() {
     private val vmFactory = BottomNavigationDrawerFragmentViewModelFactory(AppSingleton.dataRepository)
+    lateinit var activityVm: MainActivityViewModel
     lateinit var viewModel: BottomNavigationDrawerFragmentViewModel
     val usersAdapter by lazy { UserListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activityVm = ViewModelProviders.of(activity!!, MainActivityViewModelFactory(AppSingleton.dataRepository)).get(MainActivityViewModel::class.java)
         viewModel = ViewModelProviders.of(this, vmFactory).get(BottomNavigationDrawerFragmentViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.main_bottom_nav_drawer, container, false)
+
+        activityVm.getRedditClient().observe(this, Observer {
+            bottom_nav_username.text = it.second.name
+        })
 
         viewModel.getUsersList().observe(this, Observer {
             usersAdapter.submitItems(it)
